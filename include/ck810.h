01bd7128768d969d0a4e9d3a5d2beb2cc5d5cd2c
Chunming Li <chunming.li@verisilicon.com>
2018-02-02 19:24:08 +0800

    Update devices IRQ and register base address from latest spec.

--- include/ck810.h
+++ include/ck810.h
@@ -74,7 +74,7 @@
 #define CK_POWM_ADDRBASE			(volatile CK_UINT32 *)(0x10002000+PERI_BASE)
 
 /** Define DMA control base address **/
-#define CK_DMAC_CONTROL_BASEADDR	(volatile CK_UINT32 *)(0x10003000+PERI_BASE)
+#define CK_AHB_DMAC_CONTROL_BASEADDR	(volatile CK_UINT32 *)(0xFC10_0000+PERI_BASE)
 
 /****** USBD  *************/
 #define CK_USBD_ADDRBASE			(volatile CK_UINT32 *)(0x10005000+PERI_BASE)
@@ -107,44 +107,47 @@
 /*
  * define irq number of perpheral modules
  */
-#define  CK_INTC_GPIO     35
+#define  CK_INTC_MIPI	        1
+#define  CK_INTC_BT1120_N       2
+#define  CK_INTC_BT1120_ABN     3
+#define  CK_INTC_H1	            4
+#define  CK_INTC_G1	            5
+#define  CK_INTC_G2             6
+#define  CK_INTC_USB            7
+#define  CK_INTC_BUSMON         8
+#define  CK_INTC_AXIDMA0        9
+#define  CK_INTC_AXIDMA1        10
+#define  CK_INTC_AHBDMA         11 
+#define  CK_INTC_MAILBOX0       12
+#define  CK_INTC_MAILBOX1       13
+#define  CK_INTC_MAILBOX2       14
+#define  CK_INTC_MAILBOX3       15
+#define  CK_INTC_SDIO0          16
+#define  CK_INTC_SDIO1          17
+#define  CK_INTC_SDIO2          18
+#define  CK_INTC_SPI_MASTER     19
+#define  CK_INTC_SPI_SLAVE      20
+#if CONFIG_QEMU     
+#define  CK_INTC_UART0          16
+#else       
+#define  CK_INTC_UART0          21
+#endif      
+#define  CK_INTC_UART1          22
+#define  CK_INTC_UART2          23
+#define  CK_INTC_I2C0           24
+#define  CK_INTC_I2C1           25
+#define  CK_INTC_I2C2           26
+#define  CK_INTC_I2C3           27
+#define  CK_INTC_TIM0           28
+#define  CK_INTC_TIM1           29
+#define  CK_INTC_TIM2           30
+#define  CK_INTC_TIM3           31
+#define  CK_INTC_WDT0           32
+#define  CK_INTC_WDT1           33
+#define  CK_INTC_WDT2           34
+#define  CK_INTC_GPIO           35
+#define  CK_INTC_RTC            37
 
-#define  CK_INTC_TIM0      28
-#define  CK_INTC_TIM1      29
-#define  CK_INTC_TIM2      30
-#define  CK_INTC_TIM3      31
-
-#if CONFIG_QEMU
-#define  CK_INTC_UART0     16
-#else
-#define  CK_INTC_UART0     21
-#endif
-#define  CK_INTC_UART1     22
-#define  CK_INTC_UART2     23
-#define  CK_INTC_SDHC0     16
-#define  CK_INTC_SDHC1     17
-#define  CK_INTC_SSI       21
-#define  CK_INTC_I2C0      24
-#define  CK_INTC_I2C1      25
-#define  CK_INTC_I2C2      26
-#define  CK_INTC_I2C3      27
-#define  CK_INTC_WDT0      32
-#define  CK_INTC_WDT1      33
-#define  CK_INTC_WDT2      34
-#define  CK_INTC_RTC       37
-#define  CK_INTC_MIPI	     1
-#define  CK_INTC_H1	       4
-#define  CK_INTC_G1	       5
-#define  CK_INTC_G2        6
-#define  CK_INTC_USB       7
-#define  CK_INTC_BUSMON    8
-#define  CK_INTC_AXIDMA0   9
-#define  CK_INTC_AXIDMA1   10
-#define  CK_INTC_AHBDMA    11 
-#define  CK_INTC_MAILBOX0  12
-#define  CK_INTC_MAILBOX1  13
-#define  CK_INTC_MAILBOX2  14
-#define  CK_INTC_MAILBOX3  15
 
 
 /**** Timer ****/
@@ -154,9 +157,9 @@
 #define  CK_TIMER3_BASSADDR			(volatile CK_UINT32 *)(0xFC203000+PERI_BASE)
 
 /****** WDT *************/
-#define CK_WDT_ADDRBASE0				(volatile CK_UINT32 *)(0xFC204000+PERI_BASE)
-#define CK_WDT_ADDRBASE1				(volatile CK_UINT32 *)(0xFC205000+PERI_BASE)
-#define CK_WDT_ADDRBASE2				(volatile CK_UINT32 *)(0xFC206000+PERI_BASE)
+#define CK_WDT_ADDRBASE0			(volatile CK_UINT32 *)(0xFC204000+PERI_BASE)
+#define CK_WDT_ADDRBASE1			(volatile CK_UINT32 *)(0xFC205000+PERI_BASE)
+#define CK_WDT_ADDRBASE2			(volatile CK_UINT32 *)(0xFC206000+PERI_BASE)
 
 /****** PWM  *************/
 #define CK_PWM_ADDRBASE0			(volatile CK_UINT32 *)(0x10014000+PERI_BASE)
@@ -172,10 +175,16 @@
 #define CK_UART_ADDRBASE2			(volatile CK_UINT32 *)(0xFC402000+PERI_BASE)
 #endif
 
+/***** PINMUX *****/
+#define PCK_PINMUX_Control			(volatile CK_UINT32 *)(0xFC209000+PERI_BASE)
+
 /***** GPIO *****/
-#define PCK_GPIO_Control			(volatile CK_UINT32 *)(0x10019000+PERI_BASE)
+#define PCK_GPIO_Control			(volatile CK_UINT32 *)(0xFC300000+PERI_BASE)
 
 /****** SPI *************/
 #define CK_SPI_ADDRBASE0			(volatile CK_UINT32 *)(0x1001A000+PERI_BASE)
+
+/****** AXI DMA *************/
+#define PCK_AXI_DMA_Control			(volatile CK_UINT32 *)(0xFC500000+PERI_BASE)
 
 #endif /* __INCLUDE_CK5A6_H */
